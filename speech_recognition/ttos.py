4f8937789793c992e9726cabcd87d119de8d9d6c
mtbehisseste <monkey860315@gmail.com>
2018-01-15 17:40:56 +0800

    Update

--- speech_recognition/ttos.py
+++ speech_recognition/ttos.py
@@ -1,13 +1,19 @@
 # only avalible with python 2
 import pyttsx3
 
+engine = pyttsx3.init('neospeech')
+engine.setProperty('rate', 135)
+
+voices = engine.getProperty('voices')
+# for voice in voices:
+#     print (voice.id)
+# print (voices[67].id)
+engine.setProperty('voices', voices[67].id)
+
 while True:
     try:
-        engine = pyttsx3.init()
-        print ("Please input some words:")
-        x = input()
+        x = input("Please input some words : ")
         engine.say(x)
-        # engine.setProperty('rate',100) 
         engine.runAndWait()
 
     except KeyboardInterrupt:
