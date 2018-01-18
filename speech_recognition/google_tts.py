d9c06514933eb239abbdc425b19e80bb86c0ae2d
mtbehisseste <monkey860315@gmail.com>
2018-01-18 18:00:05 +0800

    Using gTTS

---
+++ speech_recognition/google_tts.py
@@ -0,0 +1,15 @@
+from gtts import gTTS
+import os
+
+while True:
+    try:
+        inputStr = input("Type here: ")
+        tts = gTTS(text = inputStr, lang = 'zh', slow = True)
+        tts.save("output.mp3")
+        os.system("mpg123 -q output.mp3")
+
+    except Exception:
+        continue
+
+    except KeyboardInterrupt:
+        break
